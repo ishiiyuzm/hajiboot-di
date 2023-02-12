ef0d500db53cf125302a6bf6fccf443bd1f7b752
ishiiyuzm <ishiiyuzm0527@gmail.com>
2023-02-12 22:38:32 +0900

    CommandLineRunnerを学んでみた

--- src/main/java/com/example/hajibootdi/Frontend.java
+++ src/main/java/com/example/hajibootdi/Frontend.java
@@ -9,16 +9,16 @@ public class Frontend {
      * 「@Autowire」を付け「DIコンテナ」がインジェクションすべきフィールドであることを示す。
      * DIコンテナは「@Autowire」を付けたフィールドに対して合致する型のオブジェクトを管理内のオブジェクトから探し出してインジェクション(依存性注入)する。(オートワイヤリング)
      */
-    @Autowired
-    ArgumentResolver argumentResolver;
-    @Autowired
-    Calculator calculator;
+    //@Autowired
+    //ArgumentResolver argumentResolver;
+    //@Autowired
+    //Calculator calculator;
 
-    public void run() {
-        System.out.print("Enter 2 number like 'a b':");
-        Argument argument = argumentResolver.resolve(System.in);
-        int result = calculator.calc(argument.getA(), argument.getB());
-        System.out.println("result = " + result);
-    }
+    //public void run() {
+    //    System.out.print("Enter 2 number like 'a b':");
+    //    Argument argument = argumentResolver.resolve(System.in);
+    //    int result = calculator.calc(argument.getA(), argument.getB());
+    //    System.out.println("result = " + result);
+    //}
 
 }
--- src/main/java/com/example/hajibootdi/HajibootDiApplication.java
+++ src/main/java/com/example/hajibootdi/HajibootDiApplication.java
@@ -1,5 +1,7 @@
 package com.example.hajibootdi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -14,18 +16,31 @@ import java.util.Scanner;
 *「@Import(AppConfig.class)」はJavaConfigを読み込む為に@Importで@Configurationを付けた対象クラスを指定(DIコンテナに登録したいBeanを1つ1つ定義するイメージ)
 *「@ComponetScan」を付けることでそのクラスのパッケージ以下を走査して「@Component」など特定のアノテーションがついてるJavaクラスを検出しそれらを「DIコンテナ」に登録する。(DIコンテナに登録したいBeanを自動で登録するイメージ)
 *「@SpringBootApplication」は「@EnableAutoConfiguration」「@Configuration」「@ComponetScan」を合成したアノテーション
+*「CommandLineRunner」インターフェースのrunメソッドが、Frontendクラスのrunメソッド相当の役割を担う
 */
 //@EnableAutoConfiguration
 //@Import(AppConfig.class)
 //@ComponentScan
 @SpringBootApplication
-public class HajibootDiApplication {
+public class HajibootDiApplication implements CommandLineRunner {
+
+	@Autowired
+    ArgumentResolver argumentResolver;
+    @Autowired
+    Calculator calculator;
+
+    public void run(String... Strings) throws Exception{
+        System.out.print("Enter 2 number like 'a b':");
+        Argument argument = argumentResolver.resolve(System.in);
+        int result = calculator.calc(argument.getA(), argument.getB());
+        System.out.println("result = " + result);
+    }
 
 	// エントリポイント
 	public static void main(String[] args) {
 
 		// 「SpringApplication.run」で「SpringBoot」アプリケーションを起動
-		ApplicationContext context = SpringApplication.run(HajibootDiApplication.class, args);
+		//ApplicationContext context = SpringApplication.run(HajibootDiApplication.class, args);
 
 		// Scanner scanner = new Scanner(System.in);
 		// System.out.print("Enter 2 numbers lile 'a b':");
@@ -42,8 +57,10 @@ public class HajibootDiApplication {
 		/*
 		 * ↑の主処理は「Frontend」クラスに集約したので以下の実装で済む(スッキリ)
 		 */
-		Frontend frontend = context.getBean(Frontend.class);
-		frontend.run();
+		//Frontend frontend = context.getBean(Frontend.class);
+		//frontend.run();
+
+		SpringApplication.run(HajibootDiApplication.class, args);
 	}
 
 }
